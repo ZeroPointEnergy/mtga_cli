afbc2261f5430513c016311dfd866026bef5d151
Andreas Zuber <zuber@puzzle.ch>
2019-07-07 02:40:55 +0200

    Add m20 set

--- lib/mtga_cli/db.rb
+++ lib/mtga_cli/db.rb
@@ -7,7 +7,7 @@ require 'json'
 
 module MtgaCli
   class Db < YAML::Store
-    MTGA_SETS = ['xln', 'rix', 'dom', 'm19', 'grn', 'rna', 'war']
+    MTGA_SETS = ['xln', 'rix', 'dom', 'm19', 'grn', 'rna', 'war', 'm20']
     EXCLUDE_CARDS = ['Forest', 'Mountain', 'Swamp', 'Island', 'Plains']
 
     def initialize
